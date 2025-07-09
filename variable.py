72793f58b586c0c309ca6fd0e5851ec9a788d3bf
bhaskarSunkari <sunkari.bhaskarrao@gmail.com>
2025-07-08 20:53:32 -0700

    varible changes

---
+++ variable.py
@@ -0,0 +1,19 @@
+import random
+
+x = 'hellow world'
+print(type(x))
+y = 30
+print(type(y))
+x = ("apple", "banana", "cherry")
+print(type(x))
+z= -3.5
+print(z)
+print(type(z))
+w=32e4
+print(type(w))
+r= 5+3j
+print(type(r))
+
+s = random.randrange(1,10)
+
+print(s)
